6f9d31c8f5b4bf2fec29b06b03acb3d703be1a31
jorgerebolledoa <aziel.az18@gmail.com>
2022-04-27 01:34:27 +0000

    todo

--- src/js/component/TODO.jsx
+++ src/js/component/TODO.jsx
@@ -1,12 +1,10 @@
 import React, { useState, useRef, useEffect } from "react";
 
 function TODO() {
-	let nombreRef = useRef(null);
-	const setSingleSong = (x, i) => {
-		nombreRef.src = `https://assets.breatheco.de/apis/sound/${x}`;
-		setsongsActive(i);
+	const crear = () => {
+		return <li>hola22</li>;
 	};
-
+	//aca debo crear un li
 	const atras = () => {
 		//aca debo borrar la li creada
 	};
@@ -14,20 +12,14 @@ function TODO() {
 		<div className="container mt-3">
 			<div className="row">
 				<div className="col-md-8 mx-auto text-center">
-					<ol className="rounded ">
-						<li>hola</li>
-					</ol>
-					<div className="row">
-						<div className="col-md-8 mx-auto text-center ">
-							<div className="mb-2">
-								<button
-									className="btn-sm  rounded-circle p-2 m-0 bg-secondary border-0 m-1"
-									onClick={atras}
-									id="boton">
-									<i class="fas fa-arrow-circle-left"></i>
-								</button>
-							</div>
-						</div>
+					<ol className="rounded bg-primary">{crear}</ol>
+					<div className="m-2">
+						<button
+							className="btn-sm  rounded-circle p-2 m-0 bg-secondary border-0 m-1"
+							onClick={atras}
+							id="boton">
+							<i class="fas fa-arrow-circle-left"></i>
+						</button>
 					</div>
 				</div>
 			</div>
