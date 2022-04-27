5e2b995a9201133dc775c34302b22495d223ef3d
jorgerebolledoa <aziel.az18@gmail.com>
2022-04-27 01:39:41 +0000

    todo

--- src/js/component/TODO.jsx
+++ src/js/component/TODO.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useRef, useEffect } from "react";
 
 function TODO() {
-	const crear = () => {
-		return <li>hola22</li>;
+	const crear = (valor) => {
+		return <li>{valor}</li>;
 	};
 	//aca debo crear un li
 	const atras = () => {
@@ -12,7 +12,7 @@ function TODO() {
 		<div className="container mt-3">
 			<div className="row">
 				<div className="col-md-8 mx-auto text-center">
-					<ol className="rounded bg-primary">{crear}</ol>
+					<ol className="rounded bg-primary">{crear(holaaa)};</ol>
 					<div className="m-2">
 						<button
 							className="btn-sm  rounded-circle p-2 m-0 bg-secondary border-0 m-1"
