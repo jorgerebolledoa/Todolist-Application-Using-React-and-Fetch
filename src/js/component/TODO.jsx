181169451d5be4a3d8ffcfcc55aad66fcfcc5caf
jorgerebolledoa <aziel.az18@gmail.com>
2022-05-02 13:56:03 +0000

    listo

--- src/js/component/TODO.jsx
+++ src/js/component/TODO.jsx
@@ -9,6 +9,19 @@ function TODO() {
 			e.target.value = "";
 		}
 	};
+	const Delete = (index) => {
+		lista.splice(index, 1);
+		setlista([...lista]);
+
+		// nuevo array
+		task.splice(index, 1);
+		setTask([...task]);
+		updateTask(urlApi, task);
+		//
+	};
+	const borrar = () => {
+		setlista([]);
+	};
 
 	return (
 		<div className="container bg-warning text-dark m-2  rounded align-items-center">
@@ -32,10 +45,27 @@ function TODO() {
 										className="list-group-item bg-warning text-dark"
 										key={index}>
 										{tastk}
+										<i
+											className="fas fa-trash float-end"
+											onClick={borrar}></i>
 									</li>
 								);
 							})}
 					</ul>
+					<div id="passwordHelpBlock" class="form-text">
+						{lista.length == 0 ? (
+							"Agrega una tarea"
+						) : (
+							<p>hay {lista.length} tareas pendientes</p>
+						)}
+					</div>
+
+					<button
+						type="button"
+						class="btn btn-dark text-warning mb-2 mt-2"
+						onClick={borrar}>
+						Borrar todo
+					</button>
 				</div>
 			</div>
 		</div>
